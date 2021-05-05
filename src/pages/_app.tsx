0d4cf888c96506e7c8faa264be1536cd8e29a0f2
clay10j <clayjoseck@yahoo.com>
2021-05-05 09:20:40 -0500

    Ignore font onLoad error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
           media="print"
+          // @ts-ignore
           onLoad="this.media='all'"
         />
         <noscript>
