b8a9d8ffbcae48bf82467539fd706e54b06805b7
clay10j <clayjoseck@yahoo.com>
2021-05-04 20:56:15 -0500

    Add work experience section and change font

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,18 +6,15 @@ export default function Skills() {
   return (
     <div className="container mx-auto my-10">
       <h1 className="m-4 text-xl md:text-5xl">Skills</h1>
-      <div className="grid grid-cols-2 justify-items-center md:grid-cols-4">
-        <DiPython
-          className="w-16 h-16 m-1 md:h-36 md:w-36 md:justify-self-end"
-          title="python"
-        />
+      <div className="grid grid-cols-2 px-2 justify-items-center md:grid-cols-4">
+        <DiPython className="w-16 h-16 m-1 md:h-36 md:w-36" title="python" />
         <DiReact className="w-16 h-16 m-1 md:h-36 md:w-36" title="react" />
         <DiJavascript1
           className="w-16 h-16 m-1 md:h-36 md:w-36"
           title="javascript"
         />
         <SiPowershell
-          className="w-16 h-16 m-1 md:h-36 md:w-36 md:justify-self-start"
+          className="w-16 h-16 m-1 md:h-36 md:w-36"
           title="powershell"
         />
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,42 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from "next/app";
+import Head from "next/head";
 import React from "react";
-import Heading from "../components/Heading";
-import Skills from "../components/Skills";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen p-4 divide-y-2 divide-white divide-opacity-60">
-      <Heading />
-      <Skills />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Hey Its Clay</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+          />
+        </noscript>
+      </Head>
+      <div className="h-screen p-4 divide-y-2 divide-white divide-opacity-60">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
