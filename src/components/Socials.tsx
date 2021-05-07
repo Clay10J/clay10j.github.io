71acfbef84cc87c50ec44910d6c8bd823587a8a2
clay10j <clayjoseck@yahoo.com>
2021-05-06 19:51:22 -0500

    Add resume download

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,6 +3,8 @@ import { FaFileDownload, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const GITHUB_LINK = "https://www.github.com/clay10j";
 const LINKEDIN_LINK = "https://www.linkedin.com/in/clayton-joseck";
+const RESUME_LINK =
+  "https://github.com/Clay10J/resume/raw/main/Clayton%20Joseck's%20Resume%202021.pdf";
 
 export default function Socials() {
   return (
@@ -18,7 +20,7 @@ export default function Socials() {
         </a>
       </div>
       <div>
-        <a href="">
+        <a href={RESUME_LINK} target="_blank" download>
           <FaFileDownload
             className="w-5 h-5 md:w-8 md:h-8"
             title="Download My Resume"
